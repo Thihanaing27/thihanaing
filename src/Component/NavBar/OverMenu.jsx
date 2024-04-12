e487be5a09a43d3b498ed24292f0091c837caccd
Thiha Naing 27 <thihanaing272023@gmail.com>
2024-04-12 12:58:08 +0630

    test: navigate

--- src/Component/NavBar/OverMenu.jsx
+++ src/Component/NavBar/OverMenu.jsx
@@ -6,18 +6,18 @@ export const OverMenu = () => {
   return (
     <div className="min-w-full min-h-screen flex justify-center items-center opacity-90 HumbergerMenu  bg-gradient-to-l from-neutral-500 to-sky-800">
       <ul className="flex flex-col gap-4 text-center">
-        <li onClick={() => navigate(0)}>
+        <li>
           <NavLink to="/thihanaing/" end>
             Home
           </NavLink>
         </li>
-        <li onClick={() => navigate(0)}>
+        <li>
           <NavLink to="/thihanaing/about">About</NavLink>
         </li>
-        <li onClick={() => navigate(0)}>
+        <li>
           <NavLink to="/thihanaing/services">Services</NavLink>
         </li>
-        <li onClick={() => navigate(0)}>
+        <li>
           <NavLink to="/thihanaing/contact">Contact</NavLink>
         </li>
       </ul>
