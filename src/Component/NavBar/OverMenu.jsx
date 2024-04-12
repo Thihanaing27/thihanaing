96213b3851a4418ddfbbe622af634331f2346ee7
Thiha Naing 27 <thihanaing272023@gmail.com>
2024-04-12 11:34:25 +0630

    fixed: OverMenu

--- src/Component/NavBar/OverMenu.jsx
+++ src/Component/NavBar/OverMenu.jsx
@@ -5,7 +5,7 @@ export const OverMenu = () => {
     <div className="min-w-full min-h-screen flex justify-center items-center opacity-90 HumbergerMenu  bg-gradient-to-l from-neutral-500 to-sky-800">
       <ul className="flex flex-col gap-4 text-center">
         <li>
-          <NavLink to="/thihanaing/">Home</NavLink>
+          <NavLink to="/thihanaing/" end>Home</NavLink>
         </li>
         <li>
           <NavLink to="/thihanaing/about">About</NavLink>
