d9e3219818587570da86a4f7e6faa7db26ae2766
Thiha Naing 27 <thihanaing272023@gmail.com>
2024-04-14 22:08:32 +0630

    added: navigate

--- src/Component/NavBar/OverMenu.jsx
+++ src/Component/NavBar/OverMenu.jsx
@@ -3,21 +3,24 @@ import "./Nav.css";
 import { useNavigate } from "react-router-dom";
 export const OverMenu = () => {
   const navigate = useNavigate();
+  const clickHander = () => {
+    navigate(0);
+  };
   return (
     <div className="min-w-full min-h-screen flex justify-center items-center opacity-90 HumbergerMenu  bg-gradient-to-l from-neutral-500 to-sky-800">
       <ul className="flex flex-col gap-4 text-center">
-        <li>
+        <li onClick={() => clickHander()}>
           <NavLink to="/thihanaing/" end>
             Home
           </NavLink>
         </li>
-        <li>
+        <li onClick={() => clickHander()}>
           <NavLink to="/thihanaing/about">About</NavLink>
         </li>
-        <li>
+        <li onClick={() => clickHander()}>
           <NavLink to="/thihanaing/services">Services</NavLink>
         </li>
-        <li>
+        <li onClick={() => clickHander()}>
           <NavLink to="/thihanaing/contact">Contact</NavLink>
         </li>
       </ul>
